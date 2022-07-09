1d2699be938d2418dbf19bcec56b066f5430edfa
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-09 18:13:42 +0200

    HeroBanner with Stack instead of Button tag

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -11,12 +11,31 @@ const HeroBanner = () => {
 			<Typography fontWeight='700' sx={{ fontSize: { lg: '44px', xs: '40px' }, cursor: 'default' }} mb='23px' mt='30px'>
 				Sweat, Smile <br /> And Repeat
 			</Typography>
-			<Typography fontSize='22px' lineHeight='35px' mb={4} sx={{ cursor: 'default' }}>
+			<Typography fontSize='22px' lineHeight='35px' sx={{ cursor: 'default' }}>
 				Check out the most effective exercises
 			</Typography>
-			<Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor: '#FF2625', padding: '10px' }}>
+			{/* <Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor: '#FF2625', padding: '10px' }}>
 				Explore Exercises
-			</Button>
+			</Button> */}
+			<Stack>
+				<a
+					href='#exercises'
+					style={{
+						marginTop: '45px',
+						textDecoration: 'none',
+						width: '200px',
+						textAlign: 'center',
+						background: '#FF2625',
+						padding: '14px',
+						fontSize: '22px',
+						textTransform: 'none',
+						color: 'white',
+						borderRadius: '4px',
+					}}
+				>
+					Explore Exercises
+				</a>
+			</Stack>
 			<Typography fontWeight={600} color='#FF2625' sx={{ opacity: 0.1, display: { lg: 'block', xs: 'none' }, cursor: 'default' }} fontSize='200px'>
 				Exercise
 			</Typography>
