e68474d49e9f0467e8939e63f855510e65ad5b3d
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-10 09:31:24 +0200

    Search Exercises Done

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -11,13 +11,13 @@ const HeroBanner = () => {
 			<Typography fontWeight='700' sx={{ fontSize: { lg: '44px', xs: '40px' }, cursor: 'default' }} mb='23px' mt='30px'>
 				Sweat, Smile <br /> And Repeat
 			</Typography>
-			<Typography fontSize='22px' lineHeight='35px' sx={{ cursor: 'default' }}>
+			<Typography fontSize='22px' lineHeight='35px' mb={4} sx={{ cursor: 'default' }}>
 				Check out the most effective exercises
 			</Typography>
-			{/* <Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor: '#FF2625', padding: '10px' }}>
+			<Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor: '#FF2625', padding: '10px' }}>
 				Explore Exercises
-			</Button> */}
-			<Stack>
+			</Button>
+			{/* <Stack>
 				<a
 					href='#exercises'
 					style={{
@@ -35,7 +35,7 @@ const HeroBanner = () => {
 				>
 					Explore Exercises
 				</a>
-			</Stack>
+			</Stack> */}
 			<Typography fontWeight={600} color='#FF2625' sx={{ opacity: 0.1, display: { lg: 'block', xs: 'none' }, cursor: 'default' }} fontSize='200px'>
 				Exercise
 			</Typography>
